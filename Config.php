091e21c44cc3777c34c347a723a838a5ef219034
Dzmitry <37154517+yanyemelchyk@users.noreply.github.com>
2018-11-16 18:03:05 +0300

    Delete Config.php

--- Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-const DB_HOST = 'localhost';
-const DB_NAME = 'myProject';
-const DB_USER = 'root';
-const DB_PASSWORD = '15july2004';
-const DB_CHAR = 'utf8';
-
-const UPLOAD_PATH = '/images/';
